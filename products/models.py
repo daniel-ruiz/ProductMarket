14d74d0d9bbf883f6f4371c5e03a240de567f589
Daniel Ruiz <danruicar@gmail.com>
2017-07-23 21:41:49 +0200

    Added Subcategory model. Refactored Category model

--- products/models.py
+++ products/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-from django.db.models import fields
 
 
 class Category(models.Model):
-    name = fields.CharField(max_length=60, blank=False, null=False, unique=True)
-    created_at = fields.DateTimeField(auto_now_add=True)
-    updated_at = fields.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=60, blank=False, null=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+
+class Subcategory(Category):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
