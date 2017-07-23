6b4c8dc2f098a7607db5c17cf8d27c2a78ca83fb
Daniel Ruiz <danruicar@gmail.com>
2017-07-23 22:22:25 +0200

    Registered models in Django admin. Changed plural forms of Category and Subcategory models

--- products/models.py
+++ products/models.py
@@ -9,9 +9,15 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
 
 class Subcategory(Category):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
+
+    class Meta:
+        verbose_name_plural = 'subcategories'
 
 
 class Product(Category):
