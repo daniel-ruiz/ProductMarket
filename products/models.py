48ea0d37c9ba615a41c2804da863b4d0ee614794
Daniel Ruiz <danruicar@gmail.com>
2017-07-23 21:27:53 +0200

    Added constraints and fixed a method in Category model

--- products/models.py
+++ products/models.py
@@ -3,9 +3,9 @@ from django.db.models import fields
 
 
 class Category(models.Model):
-    name = fields.CharField(max_length=60)
+    name = fields.CharField(max_length=60, blank=False, null=False, unique=True)
     created_at = fields.DateTimeField(auto_now_add=True)
     updated_at = fields.DateTimeField(auto_now=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
