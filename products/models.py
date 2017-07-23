219c8376ece76b124b269e6bebed64fef3341f79
Daniel Ruiz <danruicar@gmail.com>
2017-07-23 21:07:49 +0200

    Added Category model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models import fields
 
-# Create your models here.
+
+class Category(models.Model):
+    name = fields.CharField(max_length=60)
+    created_at = fields.DateTimeField(auto_now_add=True)
+    updated_at = fields.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.name
