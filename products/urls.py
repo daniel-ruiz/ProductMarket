fe426fad05f5fd5de84836d71367855d332fe417
Daniel Ruiz <danruicar@gmail.com>
2017-07-25 01:08:05 +0200

    Routed product endpoints

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 
-from products.api import CategoryListAPI
+from products.api import CategoryListAPI, ProductAPI
+
+router = DefaultRouter()
+router.register(r'products', ProductAPI, base_name='product')
 
 urlpatterns = [
-    url(r'^categories/$', CategoryListAPI.as_view())
+    url(r'^categories/$', CategoryListAPI.as_view()),
+    url(r'', include(router.urls))
 ]
