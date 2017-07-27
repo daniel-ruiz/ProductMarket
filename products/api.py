5d3903f03be944c47c15efd28ac80de7231f8fe0
Daniel Ruiz <danruicar@gmail.com>
2017-07-27 15:38:48 +0200

    Refactored function-based view into a class-based view

--- products/api.py
+++ products/api.py
@@ -1,6 +1,6 @@
-from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.generics import ListAPIView, get_object_or_404
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from products.managers import CategoryManager, ProductManager, SubcategoryManager
@@ -28,15 +28,21 @@ class ProductAPI(ModelViewSet):
     ordering_fields = ('name', 'created_at', 'updated_at')
 
 
-@api_view()
-def list_products_related_to_subcategory(request, pk):
-    subcategory = get_object_or_404(SubcategoryManager.all(), pk=pk)
-    filtered_queryset = SearchFilter().filter_queryset(request, subcategory.product_set.all(), view=list_products_related_to_subcategory)
-    filtered_queryset = OrderingFilter().filter_queryset(request, filtered_queryset, view=list_products_related_to_subcategory)
-    paginator = ProductMarketPagination()
-    paginated_data = paginator.paginate_queryset(filtered_queryset, request)
-    serializer = ProductSerializer(paginated_data, many=True)
-    return paginator.get_paginated_response(serializer.data)
+class SubcategoryProductsAPI(APIView):
 
-list_products_related_to_subcategory.search_fields = ('name',)
-list_products_related_to_subcategory.ordering_fields = ('name', 'created_at', 'updated_at')
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ('name',)
+    ordering_fields = ('name', 'created_at', 'updated_at')
+
+    def get(self, request, pk):
+        subcategory = get_object_or_404(SubcategoryManager.all(), pk=pk)
+        filtered_queryset = self.filter_queryset(subcategory.product_set.all())
+        paginator = ProductMarketPagination()
+        paginated_data = paginator.paginate_queryset(filtered_queryset, request)
+        serializer = ProductSerializer(paginated_data, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+    def filter_queryset(self, query_set):
+        for backend in self.filter_backends:
+            query_set = backend().filter_queryset(self.request, query_set, view=self)
+        return query_set
--- products/urls.py
+++ products/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
-from products.api import CategoryListAPI, ProductAPI, list_products_related_to_subcategory
+from products.api import CategoryListAPI, ProductAPI, SubcategoryProductsAPI
 
 router = DefaultRouter()
 router.register(r'products', ProductAPI, base_name='product')
 
 urlpatterns = [
     url(r'^categories/$', CategoryListAPI.as_view()),
-    url(r'^categories/(?P<pk>\d+)/products/$', list_products_related_to_subcategory),
+    url(r'^categories/(?P<pk>\d+)/products/$', SubcategoryProductsAPI.as_view()),
     url(r'', include(router.urls))
 ]
