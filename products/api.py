e0df1227e260714d5dd178d4bf8fe6bc5dc04fdf
Daniel Ruiz <danruicar@gmail.com>
2017-07-27 13:25:36 +0200

    Added custom pagination and paginated list endpoints

--- products/api.py
+++ products/api.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from products.managers import CategoryManager, ProductManager, SubcategoryManager
+from products.pagination import ProductMarketPagination
 from products.serializers import CategorySerializer, ProductSerializer
 
 
@@ -11,16 +12,20 @@ class CategoryListAPI(ListAPIView):
 
     queryset = CategoryManager.all()
     serializer_class = CategorySerializer
+    pagination_class = ProductMarketPagination
 
 
 class ProductAPI(ModelViewSet):
 
     queryset = ProductManager.all()
     serializer_class = ProductSerializer
+    pagination_class = ProductMarketPagination
 
 
 @api_view()
 def list_products_related_to_subcategory(request, pk):
     subcategory = get_object_or_404(SubcategoryManager.all(), pk=pk)
-    serializer = ProductSerializer(subcategory.product_set, many=True)
-    return Response(serializer.data)
+    paginator = ProductMarketPagination()
+    paginated_data = paginator.paginate_queryset(subcategory.product_set.all(), request)
+    serializer = ProductSerializer(paginated_data, many=True)
+    return paginator.get_paginated_response(serializer.data)
