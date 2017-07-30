c8532ecdee3f6af8225a54dc6505ebc0293c02c1
Daniel Ruiz <danruicar@gmail.com>
2017-07-30 13:02:23 +0200

    Added static file settings for production deployments

--- product_market/settings.py
+++ product_market/settings.py
@@ -122,7 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Configure database regarding the value of $DATABASE_URL
 
